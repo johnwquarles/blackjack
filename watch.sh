f138b50adaec2edb2ec6444e95d545778f390a86
johnwquarles <john.w.quarles@gmail.com>
2015-05-19 18:13:55 -0500

    moved space in header closer to text

--- watch.sh
+++ watch.sh
@@ -32,7 +32,7 @@ cp -r src public
 
 # concat bower_components to lib directory
 if [ -d "bower_components" ]; then
-  ./node_modules/.bin/bowcat . -o public/lib -m
+  ./node_modules/.bin/bowcat -o public/lib
 fi
 
 echo "clean"
